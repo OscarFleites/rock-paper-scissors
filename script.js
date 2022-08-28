e6424aef0a4a9792c9e8683efbc734aaa39532d7
Oscar <fleitesoscar97@gmail.com>
2022-08-28 19:53:55 -0400

    Restarting everything, included psuedocode on bottom explaining what to do

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function getComputerChoice() {
-  let rock, paper, scissors;
-  const random = Math.floor(Math.random() * 3 + 1);
-  if (random === 3) {
-    return (scissors = "Scissors");
-  } else if (random === 2) {
-    return (paper = "Paper");
-  } else random === 1;
-  return (rock = "Rock");
+let rock = "rock"
+let paper = "paper"
+let scissors ="scissors"
 
-  
+function getComputerChoice(){
+
 }
+
+function getUserChoice(){
+
+}
+
+function 
+
+
+
+
+
+// rock paper scissor Game
+// create function user will enter input value of rock, paper or scissors (prompt)
+// create function input value will be case sensitive string.toUpperCase
+// create function computer random values Math.floor(Math.random)
+// create function comparing user/computer value and evaluating winner
+// Have game loop until someone achieves 5 wins (while loop) 
+// while (i < 10) {
+// text += "The number is " + i; 
+// i++
